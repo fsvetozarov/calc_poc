ba796b708c7dff319d465fa725b3ba6066f79b26
Filip S <fsvetozarov@gmail.com>
2023-03-13 07:46:51 +0100

    comm

--- src/routes/components/Answers.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { writable } from "svelte/store";
-
-    let questionare = writable ([
-        {group: "General", id: 0, 
-            question: "What's your's company general purpose of implementing ICM tool?", answer: "",
-            helpbox: "Lorem-Ipsum"}
-    ])
-</script>
---
+++ src/routes/process/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import "../../app.css";
+	
+</script>
+
+<div class="">
+	
+
+	<div class = "">
+		<slot />
+	</div>
+
+	<footer>
+
+	</footer>
+</div>
+
+<style>
+	
+</style>
